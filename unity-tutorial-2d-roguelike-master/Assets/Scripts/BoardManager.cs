b7e969c000b940f4b91d2089b0c58912afd2ff52
RedAlice64 <fortestig@hotmail.com>
2016-06-24 16:11:18 -0400

    battle scene
    Battle scene for dota legend like game.

---
+++ unity-tutorial-2d-roguelike-master/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+	[Serializable]
+	public class Count
+	{
+		public int minimum;
+		public int maximum;
+
+		public Count (int min, int max)
+		{
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 1;
+	public int rows = 1;
+	public Count wallCount = new Count (1,2);
+	public Count foodCount = new Count (1,2);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] enemyTiles;
+	public GameObject[] outerWallTiles;
+
+	private Transform boardHolder;
+	private List <Vector3> gridPositions = new List <Vector3> ();
+
+	void InitialiseList()
+	{
+        columns = 8;
+        rows = 3;
+        gridPositions.Clear ();
+
+		for (int x = 1; x < columns - 1; x++) {
+			for (int y = 1; y < rows - 1; y++) {
+				gridPositions.Add (new Vector3 (x, y, 0f));
+			}
+		}
+	}
+
+	void BoardSetup ()
+	{
+        columns = 8;
+        rows = 3;
+        boardHolder = new GameObject ("Board").transform;
+
+		for (int x = -1; x < columns + 1; x++) {
+			for (int y = -1; y < rows + 1; y++) {
+				GameObject toInstantiate = floorTiles [Random.Range (0, floorTiles.Length)];
+				if (x == -1 )
+					toInstantiate = outerWallTiles [Random.Range (0, outerWallTiles.Length)];
+
+				GameObject instance = Instantiate (toInstantiate, new Vector3 (x, y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent (boardHolder);
+			}
+		}
+	}
+
+	Vector3 RandomPosition()
+	{
+        columns = 8;
+        rows = 3;
+		int randomIndex = Random.Range (0, gridPositions.Count);
+		Vector3 randomPosition = new Vector3(Random.Range(4,8),Random.Range(1,3),0.0f);
+		gridPositions.RemoveAt (randomIndex);
+		return randomPosition;
+	}
+
+	void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+	{
+		int objectCount = 3;
+
+		for (int i = 0; i < 3; i++) {
+			Vector3 randomPosition = RandomPosition ();
+			GameObject tileChoice = tileArray [Random.Range (0, tileArray.Length)];
+			Instantiate (tileChoice, randomPosition, Quaternion.identity);
+		}
+	}
+
+	public void SetupScene(int level)
+	{
+        columns = 8;
+        rows = 3;
+
+        BoardSetup ();
+		InitialiseList ();
+		//LayoutObjectAtRandom (wallTiles, wallCount.minimum, wallCount.maximum);
+		//LayoutObjectAtRandom (foodTiles, foodCount.minimum, foodCount.maximum);
+		int enemyCount = (int)Mathf.Log (level, 2f);
+		LayoutObjectAtRandom (enemyTiles, enemyCount, enemyCount);
+		//Instantiate (exit, new Vector3 (columns - 1, rows - 1, 0F), Quaternion.identity);
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ unity-tutorial-2d-roguelike-master/Assets/Scripts/GameManager.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public float levelStartDelay = 2f;
+	public float turnDelay = .5f;
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+	public int playerFoodPoints = 100;
+	[HideInInspector] public bool playersTurn = true;
+
+	private Text levelText;
+	private GameObject levelImage;
+	private int level = 1;
+	private List<Enemy> enemies;
+	private bool enemiesMoving;
+	private bool doingSetup;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy> ();
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void OnLevelWasLoaded (int index)
+	{
+		level++;
+
+		InitGame ();
+	}
+
+	void InitGame()
+	{
+		doingSetup = true;
+
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text> ();
+		levelText.text = "Start";
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
+		enemies.Clear ();
+		boardScript.SetupScene (level);
+	}
+
+	private void HideLevelImage()
+	{
+		levelImage.SetActive (false);
+		doingSetup = false;
+	}
+
+	public void GameOver()
+	{
+		levelText.text = "You lose";
+		levelImage.SetActive (true);
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playersTurn || enemiesMoving || doingSetup)
+		    return;
+        //if (enemiesMoving || doingSetup)
+          //  return;
+
+        StartCoroutine (MoveEnemies ());
+	}
+
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add (script);
+	}
+
+	IEnumerator MoveEnemies()
+	{
+		enemiesMoving = true;
+		yield return new WaitForSeconds (turnDelay);
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay);
+		}
+
+		for (int i = 0; i < enemies.Count; i++) {
+            if (enemies.Count < 1) break;
+			enemies [i].MoveEnemy ();
+            if (enemies[i].hp <= 0)
+            {
+                Enemy temp = enemies[i];
+                enemies.RemoveAt(i);
+                //temp.SetActive(false);
+                Destroy(temp);
+                i--;
+            }
+            
+            if (enemies.Count < 1) yield return new WaitForSeconds(turnDelay);
+            if(i<0)yield return new WaitForSeconds (0.5f);
+            yield return new WaitForSeconds(0.5f);
+        }
+
+		playersTurn = true;
+		enemiesMoving = false;
+	}
+
+
+}
